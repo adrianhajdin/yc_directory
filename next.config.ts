910359d9bcd71683c77c456b1b0d8f4c10f48b85
Adrian <contact@jsmastery.pro>
2024-10-15 21:53:39 +0200

    update next config

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     dangerouslyAllowSVG: true,
     remotePatterns: [
