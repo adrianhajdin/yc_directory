f6880a3573202075bf96c327e9520e3da1e24fc2
unknown <arhanansari2009@gmail.com>
2024-11-03 13:39:28 +0530

    Adding Live Preview

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -1,6 +1,18 @@
+// sanity/lib/live.ts
 import "server-only";
 
 import { defineLive } from "next-sanity";
 import { client } from "@/sanity/lib/client";
 
-export const { sanityFetch, SanityLive } = defineLive({ client });
+// set your viewer token
+const token = process.env.SANITY_API_READ_TOKEN
+if (!token) {
+  throw new Error("Missing SANITY_API_READ_TOKEN")
+}
+
+// export the sanityFetch helper and the SanityLive component
+export const { sanityFetch, SanityLive } = defineLive({
+  client,
+  serverToken: token,
+  browserToken: token,
+})
