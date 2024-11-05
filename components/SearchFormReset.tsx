455140d3da30544796343f71c68a2800d42b4ebd
harshit duggal <135515880+Harshitduggal1@users.noreply.github.com>
2024-11-05 12:01:21 +0530

    Update SearchFormReset.tsx

--- components/SearchFormReset.tsx
+++ components/SearchFormReset.tsx
@@ -1,21 +1,24 @@
-"use client"
+"use client";
 
 import Link from "next/link";
-import {X} from "lucide-react";
+import { X } from "lucide-react";
 
 const SearchFormReset = () => {
     const reset = () => {
         const form = document.querySelector('.search-form') as HTMLFormElement;
 
-        if(form) form.reset();
+        if (form) form.reset();
     }
 
     return (
-        <button type="reset" onClick={reset}>
-            <Link href="/" className="search-btn text-white">
-                <X className="size-5" />
-            </Link>
+        <button 
+            type="button" 
+            onClick={reset} 
+            className="flex items-center justify-center p-2 rounded-full bg-red-500 hover:bg-red-600 transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-red-300"
+        >
+            <X className="w-6 h-6 text-white" />
         </button>
     )
 }
-export default SearchFormReset
+
+export default SearchFormReset;
